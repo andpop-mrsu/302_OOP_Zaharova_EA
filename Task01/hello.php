994b54f5330d8ce15e9f6c3892c5587e3729bac9
Ayekazashka <pypypm21@gmail.com>
2024-06-10 23:22:10 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,6 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+$dateTime = date("d.m.Y H:i:s");
+$FIO = 'Захарова Екатерина';
+echo "Текущая дата и время: $dateTime";
+echo "\nФамилия и имя: $FIO";
---
+++ Task01/start.bat
@@ -0,0 +1,3 @@
+#!/bin/bash
+D:\MyPrograms\OOP_php\php-8.3.2-Win32-vs16-x64\php.exe C:\Users\Zakharova\302_OOP_Zaharova_EA\Task01
+pause
